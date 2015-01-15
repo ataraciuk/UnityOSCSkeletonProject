10ad57e84624e57ba0ca169266b7d47f5fff1ba5
ataraciuk <ataraciuk@gmail.com>
2015-01-14 22:42:56 -0500

    first version

---
+++ Assets/Assets/Scripts/OSCListener.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class OSCListener : MonoBehaviour {
+
+	public void OnEnable(){
+		UnityOSCReceiver.OSCMessageReceived += new UnityOSCReceiver.OSCMessageReceivedHandler(OSCMessageReceived);
+		
+	}
+	public void OnDisable(){
+		UnityOSCReceiver.OSCMessageReceived -= new UnityOSCReceiver.OSCMessageReceivedHandler(OSCMessageReceived);
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OSCMessageReceived(OSC.NET.OSCMessage message){
+		string address = message.Address; //this has the address
+		ArrayList args = message.Values;
+		/*
+		 * args is an array with received parameters. args[0] gets first one. args[1] gets second one. etc.
+		 * Cast to transform to desired type:
+		 * (int)args[0]
+		 * (float)args[1]
+		 * Use BroadcastMessage to send message to descendants
+		*/
+		Debug.Log("address: "+ address + "     |     args: "+string.Join(",",args.ToArray().Select(x => x.ToString()).ToArray()));
+	}
+}
